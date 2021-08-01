98792b272e5fb5c0fe5bd672c39b833122875e8b
hussainkazmi <59509208+hussainkazmi@users.noreply.github.com>
2021-08-01 22:57:08 +0200

    Delete Energy-data-science directory

--- Energy-data-science/Experimental/scene.py
+++
@@ -1,5 +0,0 @@
-class SquareToCircle(Scene):
-    def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
